4f7eb0834d1ca1200651b041cdd4669ceacc2b65
darklefknight <darklefknight@web.de>
2017-11-20 17:57:14 +0100

    understanding whats going on

--- Live_Quicklook_old.py
+++ Live_Quicklook_old.py
@@ -12,13 +12,10 @@ Marcus Klingebiel, Tobias Machnitzki, April 2017
 
 from netCDF4 import Dataset
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdate
 from matplotlib import gridspec, cm
 import time
-from time import gmtime, strftime
 import numpy.ma as ma
 from scipy.signal import savgol_filter
 from matplotlib.ticker import AutoMinorLocator
@@ -47,8 +44,6 @@ year_str    = yyyymmdd[0:4]
 month_str   = yyyymmdd[4:6]
 day_str     = yyyymmdd[6:8]
 
-time_now = str(strftime("%d:%m:%Y, %X UTC", gmtime()))
-print(time_now)
 #%%
 #=========================
 # # Input NETCDF Files
@@ -57,15 +52,12 @@ print(time_now)
 StartTime = 0
 EndTime   = 24
 
-nc_file = "/home/data/LIVE/MBR2/MMCR__MBR__Spectral_Moments__10s__155m-25km__"+date_str+".nc"
-nc_meteo = "/home/data/LIVE/WEATHER/Meteorology__Deebles_Point__2m_10s__"+year_str + month_str + day_str +".nc"
-image_path = "/home/data/LIVE/ALLSKY/"
 
-#nc_file = "/pool/OBS/BARBADOS_CLOUD_OBSERVATORY/Level_1/B_Reflectivity/Version_2/MMCR__MBR__Spectral_Moments__10s__155m-25km__"+date_str+".nc"
-#nc_meteo = "/pool/OBS/BARBADOS_CLOUD_OBSERVATORY/Level_1/I_Meteorology_2m/" + year_str + month_str + "/Meteorology__Deebles_Point__2m_10s__"+year_str + month_str + day_str +".nc" 
-#image_path = "/pool/OBS/BARBADOS_CLOUD_OBSERVATORY/Level_0/1_Allskyimager/data/cc" + year_str[2:4] + month_str + "/" + day_str + "/sm" + year_str[2:4] + month_str + day_str + ".tgz"
+nc_file = "/pool/OBS/BARBADOS_CLOUD_OBSERVATORY/Level_1/B_Reflectivity/Version_2/MMCR__MBR__Spectral_Moments__10s__155m-25km__"+date_str+".nc"
+nc_meteo = "/pool/OBS/BARBADOS_CLOUD_OBSERVATORY/Level_1/I_Meteorology_2m/" + year_str + month_str + "/Meteorology__Deebles_Point__2m_10s__"+year_str + month_str + day_str +".nc" 
+image_path = "/pool/OBS/BARBADOS_CLOUD_OBSERVATORY/Level_0/1_Allskyimager/data/cc" + year_str[2:4] + month_str + "/" + day_str + "/sm" + year_str[2:4] + month_str + day_str + ".tgz"
 
-save_figures = "/home/data/LIVE/PLOT/"
+save_figures = "/home/mpim/m300517/MPI/working/Quicklook_live/save_figures/"
 save_name = save_figures+'QL_Live'+year_str+month_str+day_str+'.png'
 
 temp_folder = "temp_images_folder" #Name for a tempurary created folder for extracting the images
@@ -112,10 +104,8 @@ meteo = Dataset(nc_meteo)
 
 temp = meteo.variables['T'][:].copy()
 wind_speed = meteo.variables['VEL'][:].copy()
-wind_dir = meteo.variables['DIR'][:].copy()
 rain = meteo.variables['MXRI'][:].copy()
 RH = meteo.variables['RH'][:].copy()
-R = meteo.variables['R'][:].copy()
 meteo_time = meteo.variables['time'][:].copy()
 
 rain[rain < 0.05] = 0    #Filtering the error values
@@ -141,9 +131,6 @@ temp_str = MinTemp + '\n' + MaxTemp + '\n' + DeltaTemp
                                    
 MaxRain = 'Max Precip = ' +str(np.nanmax(rain))[0:6] + ' mm/h ('+ str(time.strftime('%H:%M:%S', time.gmtime((meteo_time[TMT.nanargmax(rain)[0]])))[0:5]) + ' UTC)'
 
-MinWind = 'Min Wind = ' +str(np.nanmin(wind_speed))[0:6] + ' m/s ('+ str(time.strftime    ('%H:%M:%S', time.gmtime((meteo_time[TMT.nanargmin(wind_speed)[0]])))[0:5]) + ' UTC)'
-MaxWind = 'Max Wind = ' +str(np.nanmax(wind_speed))[0:6] + ' m/s ('+ str(time.strftime    ('%H:%M:%S', time.gmtime((meteo_time[TMT.nanargmax(wind_speed)[0]])))[0:5]) + ' UTC)'
-
 
 # %%
 #=========================
@@ -151,26 +138,16 @@ MaxWind = 'Max Wind = ' +str(np.nanmax(wind_speed))[0:6] + ' m/s ('+ str(time.st
 #=========================
 Humidity_test = 85.534
 Precip_test = 24.123 
-RSUM = sum(R)
+
 LiveTemp =  str(round(temp[-1],1)) + ' °C'
-LivePrecip =  str(round(rain[-1],1)) + ' mm h$^{-1}$'
-LiveHumid = str(round(RH[-1],1)) + ' %'
-#LivePrecip =  str(round(Precip_test,1)) + ' mm h$^{-1}$'
-#LiveHumid = str(round(Humidity_test,1)) + ' %'
-SumPrecip = str(round(RSUM,1)) + ' mm'
-LiveWindSpeed = (round(wind_speed[-1],1))
-LiveWindDir = (round(wind_dir[-1],1)-180)
-print(LiveWindSpeed, LiveWindDir)
+LivePrecip =  str(round(Precip_test,1)) + ' mm h$^{-1}$'
+LiveHumid = str(round(Humidity_test,1)) + ' %'
 
-wind_u = (1.944*LiveWindSpeed) *np.sin((np.pi/180)*(LiveWindDir))
-wind_v = (1.944*LiveWindSpeed) *np.cos((np.pi/180)*(LiveWindDir))
+live_time = mdate.num2date(meteo_secs[-1])
+updatestr = live_time.strftime("%x %X")
 
-#wind_u = str((round(wind_speed[-1],1))*np.sin((np.pi/180)*round((wind_dir[-1],1))))
-#wind_v = str((wind_speed[-1],1)*np.cos((np.pi/180)*(wind_dir[-1],1)))
-
-print(wind_u, wind_v)
-#wind_u= 30
-#wind_v = 20
+wind_u= 30
+wind_v = 20
 #%%
 #================================
 # # apply Filter on reflectivity:
@@ -196,10 +173,9 @@ EndTime=int((len(Time)/24*EndTime))
 # Apply Savitzky-Golay filter on 2m-Data for smoothing:
 #=======================================================
 
-temp = savgol_filter(temp,3,1)
-RH = savgol_filter(RH,3,1)
-wind_speed = savgol_filter(wind_speed,3,1)
-wind_dir = savgol_filter(wind_dir,11,1)
+temp = savgol_filter(temp,61,1)
+RH = savgol_filter(RH,61,1)
+wind_speed = savgol_filter(wind_speed,61,1)
 
 #%%
 #===================================
@@ -224,22 +200,20 @@ LCL = (((temp-tempd)/8) + z_0 ) * 1000  # *1000 to convert from km to m
 #===================================
 image_name_check = year_str[2:4] + month_str + day_str
 
-#if os.path.isdir(temp_folder):
-#    if len( glob.glob(temp_folder +"/*" + image_name_check + "*")) < 2:
-#        os.rmdir(temp_folder)
-#else:
-#    os.mkdir(temp_folder)
-#    
-#    tar_string = 'tar -xzvf ' + image_path  +' -C ' + temp_folder 
-#    os.system(tar_string)
-#
-#wdir = os.getcwd()
-#
-#image_dir = wdir + "/" + temp_folder + "/"
-#image_name = image_path + sorted(os.listdir(image_path))
-image_name = image_path + str(os.listdir(image_path))[2:22]
-#image_name = image_dir + sorted(os.listdir(image_dir))[720]
-print(image_name)
+if os.path.isdir(temp_folder):
+    if len( glob.glob(temp_folder +"/*" + image_name_check + "*")) < 2:
+        os.rmdir(temp_folder)
+else:
+    os.mkdir(temp_folder)
+    
+    tar_string = 'tar -xzvf ' + image_path  +' -C ' + temp_folder 
+    os.system(tar_string)
+
+wdir = os.getcwd()
+
+image_dir = wdir + "/" + temp_folder + "/"
+image_name = image_dir + sorted(os.listdir(image_dir))[720]
+
 image = Image.open(image_name)
 
 #%%
@@ -261,12 +235,12 @@ def makeFancyBox(x,y,width,height):
 print('Plot Figure')
 
 #Setting Fontsizes:
-ax_title_size = 13
-ylabel_size = 9
-cb_title_size = 9
-cb_size = 9
-box_font = 10
-legend_size = 8
+ax_title_size = 16
+ylabel_size = 11
+cb_title_size = 11
+cb_size = 11
+box_font = 11
+legend_size = 11
 
 barb_x = 73
 barb_y = 16
@@ -281,23 +255,18 @@ patches=[]
 
 ax1 = fig.add_subplot(gs1[5:20,:-40]) #Reflectivity
 ax_cb = fig.add_subplot(gs1[5:20,50:62]) #Colorbar for Reflectivity
-ax2 = fig.add_subplot(gs1[23:31,:-40], sharex = ax1) #Rain
-ax3 = fig.add_subplot(gs1[34:42,:-40], sharex = ax1) #2m Temperature
-ax4 = fig.add_subplot(gs1[45:53,:-40], sharex = ax1) #WindSpeed
-ax11 = fig.add_subplot(gs1[45:53,:-40], sharex = ax1) #WindDIR
-ax11 = ax4.twinx() #WindSpeed
+ax2 = fig.add_subplot(gs1[25:35,:-40], sharex = ax1) #Rain
+ax3 = fig.add_subplot(gs1[40:50,:-40], sharex = ax1) #2m Temperature
+#ax4 = ax3.twinx() #2m Relative Humidity
 ax5 = ax3.twinx() #dewpoint temperature
-
 ax6 = fig.add_subplot(gs1[25:50,71:94]) #Image
 ax7 = fig.add_subplot(gs1[5:45,68:100]) #Live-Data frame layer
 ax8 = fig.add_subplot(gs1[barb_y:barb_y+8,barb_x:barb_x+8]) #Live-Data-Windbarb
 ax9 = fig.add_subplot(gs1[5:45,68:100],) #Live-Data Text layer
-
 ax10 = fig.add_subplot(gs1[3:56,67:68]) #Black bar
 
-
 #Adjusting appearance for all subplots:
-axes = [ax1,ax2,ax3,ax4,ax11]
+axes = [ax1,ax2,ax3]
 date_fmt = '%H:%M'
 date_formatter = mdate.DateFormatter(date_fmt)
 
@@ -316,12 +285,11 @@ for ax in axes:
     ax.get_yaxis().set_tick_params(which='both', direction='out')
     ax.get_xaxis().set_tick_params(which='both', direction='out')
     
-ax4.set_xlabel('UTC Time',fontsize=ylabel_size)
-ax4.xaxis.set_label_position('bottom')
+ax3.set_xlabel('UTC Time',fontsize=ylabel_size)
 
 #Setting up the x_axes for all plots:
-ax4.set_xticks(np.arange(min(secs), max(secs),0.083333/2)) #Abstand xticks = 2h, 1 Minute = 0.000694
-ax4.set_xlim(min(secs),max(secs))
+ax3.set_xticks(np.arange(min(secs), max(secs),0.083333/2)) #Abstand xticks = 2h, 1 Minute = 0.000694
+ax3.set_xlim(min(secs),max(secs))
 
 #------------------------------------------------------------------------------------------------------------------------------------
 #=========================
@@ -354,11 +322,11 @@ for i in range(len(Time)):
 
 #Plot LCL:
 lcl_im, = ax1.plot(meteo_secs,LCL,color='black', ls = '--', lw=1.5, label = 'Lifting Condensation Level')
-lcl_legend = ax1.legend(loc=2, fontsize=legend_size)
+lcl_legend = ax1.legend(loc=1, fontsize=legend_size)
 
 if LegendNoData:
     grey_patch = mpatches.Patch(color='grey', label='RADAR off')
-    grey_legend = ax1.legend(handles=[lcl_im,grey_patch], fontsize=legend_size, loc=2)
+    grey_legend = ax1.legend(handles=[lcl_im,grey_patch], fontsize=legend_size, loc=1)
 
     #collorbar settings:
 ax_cb.set_xticks([])
@@ -382,7 +350,7 @@ print('Plotting Rain...')
 Legend_handle = False
 for i in range(len(meteo_secs)):
     if rain[i] > 0.1:
-        ax2.axvline(meteo_secs[i], linewidth = 1, color = 'g',alpha=0.025, label='Timeframe with precipitation')
+        ax2.axvline(meteo_secs[i], linewidth = 1, color = 'g',alpha=0.025, label='Timeframe where precipitation was measured')
         Legend_handle = True  
 
 im2, = ax2.plot(meteo_secs,rain,color='b',lw=1, label='Precipitation')
@@ -394,15 +362,14 @@ for label in ax2.xaxis.get_ticklabels():
 
     #y-axes settings:
 ax2.set_ylabel('Precipitation [mm/h]', fontsize=ylabel_size)
-ax2.set_ylim(0,max(rain))
-ax2.set_yticks(np.arange(0,max(rain)+10,5))
+ax2.set_ylim(0,50)
+ax2.set_yticks(np.arange(0,51,5))
 ax2.legend(loc='upper right', fontsize = legend_size)
-
 
     #make shaded area where Rain was measured:        
 if Legend_handle == True:
-    green_patch = mpatches.Patch(color='g',alpha = 0.2, label='Timeframe with precipitation')
-    green_legend = ax2.legend(handles=[im2,green_patch], fontsize=legend_size, loc=2)     
+    green_patch = mpatches.Patch(color='g',alpha = 0.2, label='Timeframe where precipitation was measured')
+    green_legend = ax2.legend(handles=[im2,green_patch], fontsize=legend_size, loc=1)     
 
 if np.nanmax(rain) > 50:
     rain_max = round(np.nanmax(rain)+5 , 0)
@@ -436,15 +403,11 @@ im3 = ax3.plot(meteo_secs,temp, color='red', lw=1, label='Temperature')
 ax3.set_title('2 m Temperature', fontsize=ax_title_size)
 
     #y-axes settings:
-ax3.yaxis.set_ticks(np.arange(0,50,2))    
-ax3.set_ylim(min(tempd)-2,max(temp)+2)
+ax3.yaxis.set_ticks(np.arange(-20,70,1))    
+ax3.set_ylim(min(tempd)-1,max(temp)+1)
 ax3.set_ylabel('Temperature [°C]', fontsize=ylabel_size)
 
-#make x-axe-labels invisible:
-for label in ax3.xaxis.get_ticklabels():
-     label.set_visible(False)
-
-#creating a grid for the Temperature and Dewpointtemperature:
+    #creating a grid for the Temperature and Dewpointtemperature:
 #ax3.grid(color = 'grey',lw=1)
     #Creating box with Max and Min values:
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
@@ -461,30 +424,6 @@ ax5.set_ylim(min(tempd)-1,max(temp)+1)
 for label in ax5.yaxis.get_ticklabels():
     label.set_visible(False)    #making the y-labels invisbel as they are the same as the temperature-labels
 
-## plotting DIR/SPEED
-#
-im4 = ax4.plot(meteo_secs,wind_speed, color='green', lw=1, label='Windspeed')
-ax4.set_title('2 m Wind', fontsize=ax_title_size)
-ax4.yaxis.set_ticks(np.arange(0,15,2))
-ax4.set_ylim(min(wind_speed),max(wind_speed)+5)
-ax4.set_ylabel('Windspeed [m/s]', fontsize=ylabel_size)
-#ax4.legend(loc='upper right', fontsize = legend_size)
-
-props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-plt.text(0.67, 1.15, MaxWind, transform=ax4.transAxes, fontsize=box_font, verticalalignment='top', bbox=props)
-
-im11 = ax11.plot(meteo_secs,wind_dir, color='orange', lw=1, label='Wind Direction')
-
-ax11.invert_yaxis()
-ax11.set_yticks(np.arange(0,360,45))
-ax11.set_ylim(max(wind_dir)+40,min(wind_dir)-40)
-ax11.set_ylabel('Wind Direction', fontsize=ylabel_size)
-ax11.grid(False)
-#ax11.grid(color = 'grey',lw=1,ls='--', axis='y')
-
-ax11.set_yticklabels(['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'])
-
-#ax11.legend(loc='upper right', fontsize = legend_size)
 
 ##----------------------------
 ##Relative Humidity
@@ -513,11 +452,7 @@ ax11.set_yticklabels(['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'])
 
 lns = im3+im5   #putting together all 3 axes in one list
 labs = [l.get_label() for l in lns] #Getting the label from each plot and put it in a list
-ax5.legend(lns, labs, fontsize=legend_size, loc=2) #creating one legend for the 3 axes (ax3, ax4, ax8)
-
-lns2 = im4+im11   #putting together all 3 axes in one list
-labs2 = [l.get_label() for l in lns2] #Getting the label from each plot and put it in a list
-ax11.legend(lns2, labs2, fontsize=legend_size, loc=2) #creating one legend for the 2 axes (ax4, ax11    )
+ax5.legend(lns, labs, fontsize=legend_size, loc=1) #creating one legend for the 3 axes (ax3, ax4, ax8)
 
 
 #======================================
@@ -563,19 +498,20 @@ makeFancyBox(48,55,33,15)  #Relative Humidity
 collection = PatchCollection(patches,color='white',edgecolor='black')
 ax7.add_collection(collection)
 
-#ax9.text(0, 0.97, '               Live Data           ', transform=ax9.transAxes, fontsize=box_font+7, verticalalignment='top')
-ax9.text(0.15, 0.97, (time_now), transform=ax9.transAxes, fontsize=live_number_font, verticalalignment='top')
+
+ax9.text(0, 0.97, '                    Live Data            ', transform=ax9.transAxes, fontsize=box_font+7, verticalalignment='top')
+ax9.text(0, 0.98, ('last update ' + updatestr), transform=ax9.transAxes, fontsize=box_font+7, verticalalignment='top')
+
+
 ax9.text(0.1, 0.89,("Temperature"), transform=ax9.transAxes, fontsize=box_font, verticalalignment='top',color='black')
 ax9.text(0.17, 0.83, (LiveTemp), transform=ax9.transAxes, fontsize=live_number_font, verticalalignment='top')
 
-ax9.text(0.48, 0.89,("Precipitation Sum"), transform=ax9.transAxes, fontsize=box_font, verticalalignment='top')
-print(RSUM)
-if RSUM < 100:   #if precipitation > 99.9: make the fontsize smaller to fit in the box
+ax9.text(0.48, 0.89,("Precipitation"), transform=ax9.transAxes, fontsize=box_font, verticalalignment='top')
+if Precip_test < 100:   #if precipitation > 99.9: make the fontsize smaller to fit in the box
     precip_font = 0
 else:
     precip_font = -1    
-ax9.text(0.5, 0.83, (SumPrecip), transform=ax9.transAxes, fontsize=(live_number_font + precip_font), verticalalignment='top')
-#ax9.text(0.5, 0.83, (LivePrecip), transform=ax9.transAxes, fontsize=(live_number_font + precip_font), verticalalignment='top')
+ax9.text(0.5, 0.83, (LivePrecip), transform=ax9.transAxes, fontsize=(live_number_font + precip_font), verticalalignment='top')
 
 
 ax9.text(0.48, 0.7,("Relative Humidity"), transform=ax9.transAxes, fontsize=box_font, verticalalignment='top')
@@ -590,7 +526,7 @@ ax8.axis('off')
 ax8.barbs(1,1,wind_u,wind_v,pivot='middle', length=10) # wind_u and wind_v must be in knots
 ax8.set_xlim(0,2)
 ax8.set_ylim(0,2)
-#ax8.set_facecolor('white')
+ax8.set_facecolor('white')
 
 #======================================
 #Black bar for seperation:
@@ -599,7 +535,7 @@ ax8.set_ylim(0,2)
 ax10.get_xaxis().set_visible(False)
 ax10.get_yaxis().set_visible(False)
 ax10.set_zorder(10)
-#ax10.set_facecolor('black')
+ax10.set_facecolor('black')
 ax10.plot(1)
 
 #%%
